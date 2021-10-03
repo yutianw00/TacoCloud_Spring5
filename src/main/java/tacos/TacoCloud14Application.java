3617374ee67bc5ee3ac3b792d56e25f5210cb165
王昱天 <yutianw@wangyutians-MacBook-Pro.local>
2021-10-03 16:57:51 -0400

    first commit

---
+++ src/main/java/tacos/TacoCloud14Application.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloud14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloud14Application.class, args);
+	}
+
+}
